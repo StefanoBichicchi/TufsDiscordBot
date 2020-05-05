e9ba66d5c2cacf098f6e64ca1393e4fa251a77db
StefanoBichicchi <bichicchi.stefano@gmail.com>
2020-05-05 17:10:03 +0200

    Add features

--- src/commands/help.js
+++ src/commands/help.js
@@ -7,21 +7,12 @@ module.exports = {
 	description: 'Visualizza la lista dei comandi',
 	execute: msg => {
 
-		let commands = []
-
-		Object.keys(allCommands).forEach(function(key) {
-			commands.push({
-				name: allCommands[key].name,
-				value: allCommands[key].description
-			});
-		});
-
-		console.dir("comandi", commands);
-
 		const embedMessage = new MessageEmbed()
 			.setTitle('Lista dei comandi di Tufs')
+			.setURL("https://github.com/StefanoBichicchi/TufsDiscordBot")
 			.setColor('#424bf5')
 			.setDescription('Ecco tutti i miei comandi')
+			.addField("Visulizza i comandi per esteso cliccando qui", "https://github.com/StefanoBichicchi/TufsDiscordBot/blob/master/README.md")
 			.addFields(
 				{ name: 'aiuto', value: 'Visualizza la lista comandi', inline: true},
 				{ name: 'ping', value: 'Pong! (visualizza il tuo ping)', inline: true},
@@ -29,6 +20,7 @@ module.exports = {
 				{ name: 'iniziativa', value: 'Crea una tabella iniziativa per gli scontri', inline: true},
 				{ name: 'lutmeme', value: 'Invia un meme de L\'Ultima Torcia!', inline: true},
 			)
+			.setTimestamp()
 			.setFooter('Bot creato da Stiv');
 
 		msg.channel.send(embedMessage)
